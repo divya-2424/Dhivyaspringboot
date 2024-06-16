d7132ab6097a131935b79f32a2329332a9617495
divya-2424 <145740499+divya-2424@users.noreply.github.com>
2024-06-16 18:36:34 +0530

    Add files via upload

---
+++ DhivyaSpringboot/sbazureappdemo1/src/main/java/com/example/sbazureappdemo1/Sbazureappdemo11Application.java
@@ -0,0 +1,13 @@
+package com.example.sbazureappdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbazureappdemo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbazureappdemo11Application.class, args);
+	}
+
+}
---
+++ DhivyaSpringboot/sbazureappdemo1/src/test/java/com/example/sbazureappdemo1/Sbazureappdemo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbazureappdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbazureappdemo11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
